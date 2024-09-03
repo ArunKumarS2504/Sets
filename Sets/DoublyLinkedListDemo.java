f19ff0be96808cf7147d506ede7ba4510628541d
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-09-03 20:23:23 +0530

    DoubleyLinkedList

--- Sets/DoublyLinkedListDemo.java
+++ Sets/DoublyLinkedListDemo.java
@@ -12,6 +12,30 @@ public class DoublyLinkedListDemo {
 	    list.insertAtFirst(5);
 	    System.out.print("List after inserting 9 and 5 at first: ");
 	    list.display(); // Output: 5 9
+	    
+	    // Inserting elements at specific positions
+        list.insertAtPos(1, 10);
+        System.out.print("List after inserting 10 at position 1: ");
+        list.display(); // Output: 5 10 9
+        
+        list.insertAtPos(0, 2);
+        System.out.print("List after inserting 2 at position 0: ");
+        list.display(); // Output: 2 5 10 9
+        
+        // Deleting the first element
+        list.deleteFirst();
+        System.out.print("List after deleting the first element: ");
+        list.display(); // Output: 5 10 9
+        
+        // Deleting an element at a specific position
+        list.deleteAtPos(1);
+        System.out.print("List after deleting element at position 1: ");
+        list.display(); // Output: 5 9
+        
+        // Deleting the last element
+        list.deleteLast();
+        System.out.print("List after deleting the last element: ");
+        list.display(); // Output: 5
 	}
 	
 }
