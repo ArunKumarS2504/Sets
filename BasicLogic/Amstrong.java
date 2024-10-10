5009f44e70768c0b58e4c1f17042285c38a2177f
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-10-10 11:12:05 +0530

    Amstrong number

--- BasicLogic/Amstrong.java
+++ BasicLogic/Amstrong.java
@@ -1,20 +1,32 @@
 package BasicLogic;
 
- class Amstrong {
-	 
-	 public int number(int n ) {
-		 
-		 int rem=n%10;
-		 int cube = n/10;
-		 int temp = rem*rem*rem+cube; 
-		 
-		 return temp;
-	 }
-	public static void main(String[] args) {
-		Amstrong as  = new Amstrong();
-		int n = 153;
-		int ans = as.number(n);
-		System.out.println(ans);
-	}
+class Amstrong {
 
+    // Method to check if a number is an Armstrong number
+    public boolean isArmstrong(int n) {
+        int originalNumber = n;
+        int result = 0;
+
+        // Loop to calculate the sum of cubes of the digits
+        while (n != 0) {
+            int rem = n % 10;         // Get the last digit
+            result += rem * rem * rem; // Add the cube of the digit
+            n = n / 10;               // Remove the last digit
+        }
+
+        // Check if the original number is equal to the result
+        return result == originalNumber;
+    }
+
+    public static void main(String[] args) {
+        Amstrong as = new Amstrong();
+        int n = 153; // Example number
+
+        // Check if the number is an Armstrong number
+        if (as.isArmstrong(n)) {
+            System.out.println(n + " is an Armstrong number.");
+        } else {
+            System.out.println(n + " is not an Armstrong number.");
+        }
+    }
 }
