b17d2229395f0d4dea0043bfa9ce82bcdfc6d4cb
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-10-13 09:06:21 +0530

    min max

--- ArrayProblems/MaxMin.java
+++ ArrayProblems/MaxMin.java
@@ -6,13 +6,21 @@ public class MaxMin {
 		
 		int[] arr = {3,4,5,2,4,7,7,4};
 		int max = arr[0];		
-		for(int i=0;i<arr.length;i++) {
+		for(int i=1;i<=arr.length-1;i++) {
 			if(arr[i]>max) {
 				max = arr[i];
 			}
 		}
 		System.out.println("Max element is" + max);
 		
+		int min = arr[0];
+		
+		for(int i=1;i<=arr.length-1;i++) {
+			if(arr[i]<min) {
+				min = arr[i];
+			}
+		}
+		System.out.println("Minimum element was :"+ min);
 	}
 
 
